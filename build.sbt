ef824a914e3af4ada314b7dd7c31d05b2e278e9b
Philippe Vinchon <p.vinchon@gmail.com>
2023-02-09 20:50:03 +0000

    Playwright getting started

--- build.sbt
+++ build.sbt
@@ -8,5 +8,6 @@ lazy val root = project
 
     scalaVersion := scala3Version,
 
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
+    libraryDependencies += "com.microsoft.playwright" % "playwright" % "1.30.0",
+    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test,
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,14 @@
-@main def hello: Unit = 
+import com.microsoft.playwright.*
+import java.nio.file.Paths
+
+@main def hello: Unit =
   println("Hello world!")
   println(msg)
+
+  val playwright = Playwright.create()
+  val browser = playwright.webkit().launch()
+  val page = browser.newPage()
+  page.navigate("http://whatsmyuseragent.org/")
+  page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")))
 
 def msg = "I was compiled by Scala 3. :)"
