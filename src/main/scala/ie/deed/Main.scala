94b2020f3fa1467f528a34e562d04783f788b2d2
Philippe Vinchon <p.vinchon@gmail.com>
2023-02-14 17:49:11 +0000

    Add a generic type for all and implemented SherryFitzIe

--- src/main/scala/ie/deed/Main.scala
+++ src/main/scala/ie/deed/Main.scala
@@ -1,6 +1,6 @@
 package ie.deed
 
-import ie.deed.websites.*
+import ie.deed.websites.SherryFitzIe
 import zio.http.*
 import scala.util.chaining.scalaUtilChainingOps
 
@@ -10,12 +10,13 @@ import scala.util.chaining.scalaUtilChainingOps
 
   zio.Unsafe.unsafe { implicit unsafe =>
     val config = ClientConfig.empty.requestDecompression(true)
-    val run = ie.sherryfitz
-      // ie.dng
-      .scrape()
+    val run = SherryFitzIe.scrape.runCount
       .provide(ClientConfig.live(config), Client.fromConfig)
 
     zio.Runtime.default.unsafe.run(run).pipe(println)
   }
 
 def msg = "I was compiled by Scala 3. :)"
+
+// https://www.citizensinformation.ie/en/consumer/phone_internet_tv_and_postal_services/eircode.html
+val eircodeRegex = "([A-Z][0-9]{2}|D6W) ?[A-Z0-9]{4}".r
---
+++ src/main/scala/ie/deed/websites/SherryFitzIe.scala
@@ -0,0 +1,120 @@
+package ie.deed.websites
+
+import ie.deed.{Scraper, Record, eircodeRegex}
+import collection.convert.ImplicitConversions.*
+import util.chaining.scalaUtilChainingOps
+import zio.{ZIO, Console}
+import zio.stream.ZStream
+import zio.json._
+import zio.http.model.Headers
+import zio.http.{Client, ZClient, Body, Response}
+import org.jsoup.Jsoup
+import java.time.Instant
+
+private case class PropertiesApiResponse(
+    features: List[PropertiesApiResponseFeature]
+)
+object PropertiesApiResponse {
+  implicit val decoder: JsonDecoder[PropertiesApiResponse] =
+    DeriveJsonDecoder.gen[PropertiesApiResponse]
+}
+private case class PropertiesApiResponseFeature(
+    properties: PropertiesApiResponseFeatureProperty
+)
+object PropertiesApiResponseFeature {
+  implicit val decoder: JsonDecoder[PropertiesApiResponseFeature] =
+    DeriveJsonDecoder.gen[PropertiesApiResponseFeature]
+}
+private case class PropertiesApiResponseFeatureProperty(link: String)
+object PropertiesApiResponseFeatureProperty {
+  implicit val decoder: JsonDecoder[PropertiesApiResponseFeatureProperty] =
+    DeriveJsonDecoder.gen[PropertiesApiResponseFeatureProperty]
+}
+
+private val getPropertiesApiLinks =
+  ZStream
+    .iterate(1)(_ + 1)
+    .map { page =>
+      s"https://www.sherryfitz.ie/sfdev/api/properties/?type=all&sort=created_at&order=desc&page=$page"
+    }
+
+private def getPropertiesFromApi(
+    url: String
+): ZIO[Client, Throwable, PropertiesApiResponse] =
+  val refererHeader = Headers("Referer", "https://www.sherryfitz.ie/")
+
+  Client
+    .request(url, headers = refererHeader)
+    .retryN(3)
+    .flatMap { _.body.asString }
+    .flatMap {
+      _.fromJson[PropertiesApiResponse].left
+        .map(Throwable(_))
+        .pipe(ZIO.fromEither)
+    }
+
+private def getPropertyHtmlLink(feature: PropertiesApiResponseFeature): String =
+  s"https://www.sherryfitz.ie/${feature.properties.link}"
+
+private def getPropertyFromHtml(
+    url: String
+): ZIO[Client, Throwable, Option[Record]] =
+  Client
+    .request(url)
+    .retryN(3)
+    .flatMap { _.body.asString }
+    .flatMap { html => ZIO.attempt { Jsoup.parse(html) } }
+    .map { doc =>
+      val advertUrl = url
+
+      val advertPrice = doc
+        .select(".property-price")
+        .text
+        .filter(_.isDigit)
+        .toIntOption
+
+      val propertyEircode = doc
+        .select(".property-address h1")
+        .text
+        .pipe(eircodeRegex.findFirstIn)
+        .map(_.filter(_.isLetterOrDigit))
+
+      val propertyImageUrls = doc
+        .select(".property-image-element img")
+        .map { _.attr("src") }
+        .toList
+
+      val contactName = doc.select(".agent-card-details-name a").text
+      val contactPhone = doc.select(".agent-card-details-contact-phone a").text
+      val contactEmail = doc.select(".agent-card-details-contact-email a").text
+
+      advertPrice
+        .zip(propertyEircode)
+        .map { (advertPrice, propertyEircode) =>
+          Record(
+            at = Instant.now(),
+            advertUrl = advertUrl,
+            advertPrice = advertPrice,
+            propertyEircode = propertyEircode,
+            propertyImageUrls = propertyImageUrls,
+            contactName = contactName,
+            contactPhone = contactPhone,
+            contactEmail = contactEmail
+          )
+        }
+    }
+
+object SherryFitzIe extends Scraper:
+  val scrape = getPropertiesApiLinks
+    .mapZIOPar(5) { getPropertiesFromApi }
+    .map { _.features }
+    .takeWhile { _.nonEmpty }
+    .flattenIterables
+    .map { getPropertyHtmlLink }
+    .mapZIOPar(5) { url =>
+      getPropertyFromHtml(url)
+        .tapSome { case None =>
+          Console.printLineError(s"Failed to parse SherryFitzIe: $url")
+        }
+    }
+    .collectSome
