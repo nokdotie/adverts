b4bae59fb1a3283207d66361a31a563951bff308
Philippe Vinchon <p.vinchon@gmail.com>
2023-06-27 20:22:26 +0100

    Upload the file twice, timestamp + latest

--- common/src/main/scala/ie/deed/adverts/package.scala
+++ common/src/main/scala/ie/deed/adverts/package.scala
@@ -17,40 +17,42 @@ private def getBlobName(
 ): String =
   s"adverts/$service/$fileNameWithoutExtension.jsonl"
 
-private def getTimestampBlobName(service: String): String =
+private def getTimestampBlobName(service: String, instant: Instant): String =
   DateTimeFormatter
     .ofPattern("yyyyMMddHHmmss")
     .withZone(ZoneOffset.UTC)
-    .format(Instant.now)
+    .format(instant)
     .pipe { getBlobName(service, _) }
 
 private def getLatestBlobName(service: String): String =
   getBlobName(service: String, "latest")
 
-private def writeToTempFileScoped[R](
+private def writeToTempFile[R](
     stream: ZStream[R, Throwable, Advert]
 ): ZIO[R & Scope, Throwable, Path] = for {
   path <- Files.createTempFileScoped()
   _ <- writeJsonLinesAs(path.toFile, stream)
 } yield path
 
+private def uploadToGcpStorage(
+    path: Path,
+    blobName: String
+): ZIO[Storage, Throwable, Unit] = for {
+  bucket <- AdvertStoreImpl.bucket
+  blobInfo = BlobId
+    .of(bucket, blobName)
+    .pipe(BlobInfo.newBuilder)
+    .build()
+
+  _ <- createFrom(blobInfo, path.toFile.toPath, List.empty)
+} yield ()
+
 def writeToGcpStorate[R](
     service: String,
     stream: ZStream[R, Throwable, Advert]
 ): ZIO[R & Scope & Storage, Throwable, Unit] = for {
-  path <- writeToTempFileScoped(stream)
+  path <- writeToTempFile(stream)
 
-  bucket <- AdvertStoreImpl.bucket
-
-  _ <- BlobId
-    .of(bucket, getLatestBlobName(service))
-    .pipe(BlobInfo.newBuilder)
-    .build()
-    .pipe { createFrom(_, path.toFile.toPath, List.empty) }
-
-  _ <- BlobId
-    .of(bucket, getTimestampBlobName(service))
-    .pipe(BlobInfo.newBuilder)
-    .build()
-    .pipe { createFrom(_, path.toFile.toPath, List.empty) }
+  _ <- uploadToGcpStorage(path, getLatestBlobName(service))
+  _ <- uploadToGcpStorage(path, getTimestampBlobName(service, Instant.now))
 } yield ()
