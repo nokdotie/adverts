a3a819e17bdec474d3b562ac0720fb4e1a4cdb85
Philippe Vinchon <p.vinchon@gmail.com>
2023-07-28 09:46:56 +0100

    Filter by coordinates directly

--- adverts/src/main/scala/ie/nok/adverts/stores/AdvertFilter.scala
+++ adverts/src/main/scala/ie/nok/adverts/stores/AdvertFilter.scala
@@ -1,6 +1,7 @@
 package ie.nok.adverts.stores
 
 import ie.nok.adverts.Advert
+import ie.nok.geographic.Coordinates
 import math.Ordered.orderingToOrdered
 
 enum AdvertFilter {
@@ -10,6 +11,7 @@ enum AdvertFilter {
 
   case PriceInEur(filter: NumericFilter[Int])
   case Address(filter: StringFilter)
+  case Coordinates(filter: CoordinatesFilter)
   case SizeInSqtMtr(filter: NumericFilter[BigDecimal])
   case BedroomsCount(filter: NumericFilter[Int])
   case BathroomsCount(filter: NumericFilter[Int])
@@ -21,6 +23,7 @@ enum AdvertFilter {
 
     case PriceInEur(filter)     => filter.filter(value.advertPriceInEur)
     case Address(filter)        => filter.filter(value.propertyAddress)
+    case Coordinates(filter)    => filter.filter(value.propertyCoordinates)
     case SizeInSqtMtr(filter)   => filter.filter(value.propertySizeInSqtMtr)
     case BedroomsCount(filter)  => filter.filter(value.propertyBedroomsCount)
     case BathroomsCount(filter) => filter.filter(value.propertyBathroomsCount)
@@ -32,8 +35,8 @@ enum StringFilter {
   case And(head: StringFilter, tail: StringFilter*)
   case Or(head: StringFilter, tail: StringFilter*)
 
-  case Contains(filter: String) extends StringFilter
-  case StartsWith(filter: String) extends StringFilter
+  case Contains(filter: String)
+  case StartsWith(filter: String)
 
   def filter(value: String): Boolean = this match {
     case Empty                => true
@@ -76,4 +79,22 @@ object NumericFilter {
     )
   def LessThanOrEqual[A: Numeric](filter: A): NumericFilter[A] = NumericFilter
     .Or(NumericFilter.LessThan(filter), NumericFilter.Equals(filter))
+}
+
+enum CoordinatesFilter {
+  case Empty
+  case And(head: CoordinatesFilter, tail: CoordinatesFilter*)
+  case Or(head: CoordinatesFilter, tail: CoordinatesFilter*)
+
+  case WithinSquare(northEast: Coordinates, southWest: Coordinates)
+
+  def filter(value: Coordinates): Boolean = this match {
+    case Empty                => true
+    case And(head, tail @ _*) => (head +: tail).forall(_.filter(value))
+    case Or(head, tail @ _*)  => (head +: tail).exists(_.filter(value))
+
+    case WithinSquare(northEast, southWest) =>
+      value.latitude <= northEast.latitude && value.latitude >= southWest.latitude &&
+      value.longitude <= northEast.longitude && value.longitude >= southWest.longitude
+  }
 }
