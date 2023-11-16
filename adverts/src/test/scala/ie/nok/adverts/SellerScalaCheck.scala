82d187e97c2094c4b714bb3473a8a6aeb6db3600
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2023-11-16 12:38:50 +0000

    Seller phone is optional, formatting (#86)

--- adverts/src/test/scala/ie/nok/adverts/SellerScalaCheck.scala
+++ adverts/src/test/scala/ie/nok/adverts/SellerScalaCheck.scala
@@ -5,7 +5,7 @@ import org.scalacheck.{Arbitrary, Gen}
 val genSeller: Gen[Seller] = for {
   sellerId <- arbitrary[Int]
   name <- arbitrary[String]
-  phone <- arbitrary[String]
+  phone <- arbitrary[Option[String]]
   alternativePhone <- arbitrary[Option[String]]
   address <- arbitrary[Option[String]]
   licenseNumber <- arbitrary[Option[String]]
