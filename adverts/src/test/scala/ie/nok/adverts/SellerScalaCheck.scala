0a0b78d0dc6a0cc2174f7c7924e88e578ef8ca77
Philippe Vinchon <p.vinchon@gmail.com>
2023-11-19 13:43:10 +0000

    Rename attributes

--- adverts/src/test/scala/ie/nok/adverts/SellerScalaCheck.scala
+++ adverts/src/test/scala/ie/nok/adverts/SellerScalaCheck.scala
@@ -3,19 +3,17 @@ import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.{Arbitrary, Gen}
 
 val genSeller: Gen[Seller] = for {
-  sellerId         <- arbitrary[String]
-  name             <- arbitrary[String]
-  phone            <- arbitrary[Option[String]]
-  alternativePhone <- arbitrary[Option[String]]
-  address          <- arbitrary[Option[String]]
-  licenseNumber    <- arbitrary[Option[String]]
+  sellerId                                         <- arbitrary[String]
+  name                                             <- arbitrary[String]
+  phoneNumbers                                     <- arbitrary[List[String]]
+  physicalAddress                                  <- arbitrary[Option[String]]
+  propertyServicesRegulatoryAuthorityLicenceNumber <- arbitrary[Option[String]]
 } yield Seller(
   sellerId = sellerId,
   name = name,
-  phone = phone,
-  alternativePhone = alternativePhone,
-  address = address,
-  licenceNumber = licenseNumber
+  phoneNumbers = phoneNumbers,
+  physicalAddress = physicalAddress,
+  propertyServicesRegulatoryAuthorityLicenceNumber = propertyServicesRegulatoryAuthorityLicenceNumber
 )
 
 given Arbitrary[Seller] = Arbitrary(genSeller)
