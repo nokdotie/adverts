1ca9196a36cf9cdef27b13dc0c7fcae902ac83ac
Philippe Hosey-Vinchon <p.vinchon@gmail.com>
2023-12-31 21:34:35 +0000

    Advert lists to display an interface like netflix (#99)
    * Advert lists to display an interface like netflix
    
    * Fix CI

---
+++ adverts/src/main/scala/ie/nok/adverts/lists/AdvertList.scala
@@ -0,0 +1,14 @@
+package ie.nok.adverts.lists
+
+import ie.nok.adverts.stores.AdvertFilter
+import zio.json.{DeriveJsonCodec, JsonCodec}
+
+case class AdvertList(
+    identifier: String,
+    label: String,
+    filter: AdvertFilter
+)
+
+object AdvertList {
+  given JsonCodec[AdvertList] = DeriveJsonCodec.gen[AdvertList]
+}
