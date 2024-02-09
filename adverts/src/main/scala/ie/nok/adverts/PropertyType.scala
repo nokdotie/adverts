01288dd848cac36de5a502b36ffc0612423a9c59
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2024-02-09 23:50:52 +0000

    Adding propertyType to PropertyPalComAdvert

--- adverts/src/main/scala/ie/nok/adverts/PropertyType.scala
+++ adverts/src/main/scala/ie/nok/adverts/PropertyType.scala
@@ -10,6 +10,7 @@ enum PropertyType {
   case Detached
   case Duplex
   case EndOfTerrace
+  case House
   case SemiDetached
   case Site
   case Terraced
@@ -20,15 +21,22 @@ object PropertyType {
   import PropertyType.*
 
   def tryFromString(value: String): Try[PropertyType] = value match {
-    case "Apartment"      => Success(Apartment)
-    case "Bungalow"       => Success(Bungalow)
-    case "Detached"       => Success(Detached)
-    case "Duplex"         => Success(Duplex)
-    case "End of Terrace" => Success(EndOfTerrace)
-    case "Semi-D"         => Success(SemiDetached)
-    case "Site"           => Success(Site)
-    case "Terrace"        => Success(Terraced)
-    case unknown          => Failure(Exception(s"Unknown rating: $unknown"))
+    case "Apartment"                  => Success(Apartment)
+    case "Bungalow"                   => Success(Bungalow)
+    case "Detached Bungalow and Land" => Success(Bungalow)
+    case "Detached"                   => Success(Detached)
+    case "Detached House"             => Success(Detached)
+    case "Detached House and Land"    => Success(Detached)
+    case "Duplex"                     => Success(Duplex)
+    case "End of Terrace"             => Success(EndOfTerrace)
+    case "End-terrace House"          => Success(EndOfTerrace)
+    case "House and Land"             => Success(House)
+    case "Semi-D"                     => Success(SemiDetached)
+    case "Semi-detached House"        => Success(SemiDetached)
+    case "Site"                       => Success(Site)
+    case "Terrace"                    => Success(Terraced)
+    case "Mid-terrace House"          => Success(Terraced)
+    case unknown                      => Failure(Exception(s"Unknown propertyType: $unknown"))
   }
   given JsonCodec[PropertyType] = DeriveJsonCodec.gen[PropertyType]
 }
