37d07494a9f25ede1d3801c355d64456644d5bae
Philippe Hosey-Vinchon <p.vinchon@gmail.com>
2024-03-07 18:35:57 +0000

    Seo (#123)
    * Add aggregated folder for comparison
    
    * Method to read yesterday's adverts
    
    * POC to compare logs before calling Google
    
    * Trigger SEO after aggregation
    
    * Format

--- adverts/src/main/scala/ie/nok/adverts/stores/AdvertStoreImpl.scala
+++ adverts/src/main/scala/ie/nok/adverts/stores/AdvertStoreImpl.scala
@@ -4,6 +4,7 @@ import ie.nok.adverts.{Advert, AdvertService}
 import ie.nok.stores.compose.ZFileAndGoogleStorageStore
 import ie.nok.stores.google.storage.StorageConvention
 import java.time.{Instant}
+import java.time.temporal.ChronoUnit
 import scala.util.chaining.scalaUtilChainingOps
 import zio.stream.ZStream
 import zio.{ZIO, ZLayer}
@@ -11,6 +12,13 @@ import zio.{ZIO, ZLayer}
 object AdvertStoreImpl {
   private val blobNameLatest: String =
     StorageConvention.blobNameLatest("adverts", ".jsonl")
+
+  private val blobNameAggregatedPrefix = "adverts/aggregated"
+  private val blobNameAggregatedLatest: String =
+    StorageConvention.blobNameLatest(blobNameAggregatedPrefix, ".jsonl")
+
+  private val blobNameAggregatedVersioned: String =
+    StorageConvention.blobNameVersioned(blobNameAggregatedPrefix, Instant.now, ".jsonl")
 
   private def blobNameLatestForService(service: AdvertService): String =
     StorageConvention.blobNameLatest(s"adverts/${service.host}", ".jsonl")
@@ -22,8 +30,11 @@ object AdvertStoreImpl {
       stream: ZStream[R, Throwable, Advert]
   ): ZIO[R & ZFileAndGoogleStorageStore[Advert], Throwable, Unit] =
     ZFileAndGoogleStorageStore.write[R, Advert](
-      StorageConvention.bucketName,
-      blobNameLatest,
+      List(
+        (StorageConvention.bucketName, blobNameLatest),
+        (StorageConvention.bucketName, blobNameAggregatedLatest),
+        (StorageConvention.bucketName, blobNameAggregatedVersioned)
+      ),
       stream
     )
 
@@ -46,6 +57,21 @@ object AdvertStoreImpl {
       .read[Advert](StorageConvention.bucketName, blobName)
       .runCollect
       .map { _.toList }
+
+  protected[adverts] val readAndDecodeYesterday: ZIO[ZFileAndGoogleStorageStore[Advert], Throwable, List[Advert]] = {
+    val yesterday = Instant.now.minus(1, ChronoUnit.DAYS)
+    val blobNameGlob = StorageConvention
+      .blobNameVersionedGlobPatternForDay(blobNameAggregatedPrefix, yesterday, ".jsonl")
+
+    ZFileAndGoogleStorageStore
+      .listBlobNames[Advert](StorageConvention.bucketName, blobNameGlob)
+      .runCollect
+      .map { _.maxOption }
+      .flatMap {
+        case Some(blobName) => readAndDecode(blobName)
+        case None           => ZIO.succeed(List.empty)
+      }
+  }
 
   protected[adverts] val readAndDecodeLatest: ZIO[ZFileAndGoogleStorageStore[Advert], Throwable, List[Advert]] =
     readAndDecode(blobNameLatest)
