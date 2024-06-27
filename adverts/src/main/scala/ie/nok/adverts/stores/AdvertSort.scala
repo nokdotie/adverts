68a370a4cea1f37b9871e0ace96d324da5ada70b
Philippe Hosey-Vinchon <p.vinchon@gmail.com>
2024-06-27 15:53:46 +0100

    Add json codec and default value (#158)

--- adverts/src/main/scala/ie/nok/adverts/stores/AdvertSort.scala
+++ adverts/src/main/scala/ie/nok/adverts/stores/AdvertSort.scala
@@ -1,12 +1,17 @@
 package ie.nok.adverts.stores
 
 import ie.nok.adverts.Advert
-import ie.nok.unit.Direction
+import ie.nok.unit.{Direction, given}
 import java.time.Instant
 import scala.util.chaining.scalaUtilChainingOps
+import zio.json.{DeriveJsonCodec, JsonCodec}
 
 enum AdvertSortableField {
   case CreatedAt
+}
+
+object AdvertSortableField {
+  given JsonCodec[AdvertSortableField] = DeriveJsonCodec.gen[AdvertSortableField]
 }
 
 case class AdvertSort(
@@ -24,4 +29,10 @@ case class AdvertSort(
         case Direction.Ascending  => ordering
         case Direction.Descending => ordering.reverse
     }
+}
+
+object AdvertSort {
+  val default = AdvertSort(AdvertSortableField.CreatedAt, Direction.Descending)
+
+  given JsonCodec[AdvertSort] = DeriveJsonCodec.gen[AdvertSort]
 }
